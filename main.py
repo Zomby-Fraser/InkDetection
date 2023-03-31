4060c078c8fc185e15623c8eab0bdf72fcb581b6
Zomby Fraser <agf4@njit.edu>
2023-03-31 15:02:03 -0400

    initial upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import glob
+import PIL.Image as Image
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from tqdm import tqdm
+from ipywidgets import interact, fixed
+
+PREFIX = '/kaggle/input/vesuvius-challenge/train/1/'
+BUFFER = 30  # Buffer size in x and y direction
+Z_START = 27 # First slice in the z direction to use
+Z_DIM = 10   # Number of slices in the z direction
+TRAINING_STEPS = 30000
+LEARNING_RATE = 0.03
+BATCH_SIZE = 32
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+plt.imshow(Image.open(PREFIX+"ir.png"), cmap="gray")
